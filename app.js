7386d34c5e26e4533c03a8552c6bf8a3afc161b5
Couby <couby40@gmail.com>
2016-01-01 19:22:18 +0100

    Ajout code serveur

--- app.js
+++ app.js
@@ -22,19 +22,51 @@
 // =========================================================================================================
 
 
-// ---------------------------------
+// ----------------------------------
 // Partie initialisation de l'appli
-// ---------------------------------
+// ----------------------------------
 
-// Librairies
+// Librairies nécessaires au traitement d'un log
 var fs = require('fs'),
     readline = require('readline'),
     mongoose = require("mongoose");
+
+// Librairies pour la partie serveur
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var compress = require('compression');
+var favicon = require('serve-favicon');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var errorHandler = require('errorhandler');
+var lusca = require('lusca');
+var methodOverride = require('method-override');
+var _ = require('lodash');
+var MongoStore = require('connect-mongo')(session);
+var flash = require('express-flash');
+var path = require('path');
+var passport = require('passport');
+var expressValidator = require('express-validator');
+var sass = require('node-sass-middleware');
+
 
 // -------------
 // Controllers
 // -------------
 var debriefMissionController = require('./controllers/debriefMission');
+var homeController = require('./controllers/home');
+
+
+// ----------------------------------
+// Clés API et paramétrage Passport
+// ----------------------------------
+
+
+// -----------------------------
+// Création du serveur Express
+// -----------------------------
+var app = express();
 
 
 // ----------------
@@ -47,22 +79,92 @@ mongoose.connection.on('error', function () {
 });
 
 
-// ---------------------------------
+// --------------------------------
+// Paramétrage du serveur Express
+// --------------------------------
+app.set('port', process.env.PORT || 3001);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(compress());
+app.use(sass({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  debug: true,
+  outputStyle: 'expanded'
+}));
+app.use(logger('dev'));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressValidator());
+app.use(methodOverride());
+app.use(cookieParser());
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: secrets.sessionSecret,
+  store: new MongoStore({ url: secrets.db, autoReconnect: true })
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+app.use(lusca({
+  csrf: true,
+  xframe: 'SAMEORIGIN',
+  xssProtection: true
+}));
+app.use(function(req, res, next) {
+  res.locals.user = req.user;
+  next();
+});
+app.use(function(req, res, next) {
+  if (/api/i.test(req.path)) {
+    req.session.returnTo = req.path;
+  }
+  next();
+});
+app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
+
+
+// ---------------------------------------
+// Les routes statiques de l'application
+// ---------------------------------------
+app.get('/', homeController.index);
+
+
+// ---------------------
+// Gestion des erreurs
+// ---------------------
+app.use(errorHandler());
+
+
+// ----------------------------------
+// Mise en route du serveur Express
+// ----------------------------------
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
+});
+
+
+// -----------------------------
 // Traitement d'un fichier log
-// ---------------------------------
+// -----------------------------
 
 // Détection d'un nouveau fichier à traiter
 //debriefMissionController.detectionFichier();
 
+// Analyse du log -> BDD
 debriefMissionController.analyseFichier('log1.lua');
 
-// Suppression du fichier log analysé
+// Suppression du fichier log analysé - ou déplacement dans un répertoire des logs traités ?
 //debriefMissionController.suppressionFichier();
 
 
-// -----------------------------------
+// ------------------------------------
 // Diffusion des stats en format JSON
-// -----------------------------------
+// ------------------------------------
 // partie REST de l'appli via routes express
 
 
+
+module.exports = app;
