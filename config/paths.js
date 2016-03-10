e6ec5e423794bf385a71bb23866b5fef4e934f74
Couby <couby40@gmail.com>
2016-03-10 10:27:24 +0100

    Ajout fichier config pour définir chemin vers fichiers logs SLMod

---
+++ config/paths.js
@@ -0,0 +1,13 @@
+/*
+Chemins à paramétrer pour l'application
+
+*/
+
+module.exports = {
+
+  // Chemin vers les fichiers logs SLMod
+  slmod: process.env.SLMOD_LOG_PATH || '/SLMod_logs'
+
+  }
+
+};
